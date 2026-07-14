6067774be12a0afae253d75841925349bc16eca5
Wana Meshack Teboho <88057845+Teboho-Wana10@users.noreply.github.com>
2026-07-14 11:14:21 +0200

    Update the main.c file

--- src/main.c
+++ src/main.c
@@ -2,16 +2,57 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/cm3/systick.h>
 
-static void delay(volatile uint32_t n) {
-    while (n--) __asm__("nop");
+#define LED_PORT (GPIOA)
+#define LED_PIN (GPIO5)
+
+#define CPU_FREQ (84000000)
+#define SYSTICK_FREQ (1000)
+
+volatile uint64_t ticks = 0;
+void sys_tick_handler(void){
+    ticks++;
 }
 
-int main(void) {
-    rcc_periph_clock_enable(RCC_GPIOA);
-    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+static uint64_t get_ticks(){
+    return ticks;
+}
 
-    while (1) {
-        gpio_toggle(GPIOA, GPIO5);
-        delay(1000000);
+static void rcc_setup(void){
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
+}
+
+static void gpio_setup(void)
+{
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_mode_setup(LED_PORT,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,LED_PIN);
+}
+
+static void systick_setup(void){
+    systick_set_frequency(SYSTICK_FREQ,CPU_FREQ);
+    systick_counter_enable();
+    systick_interrupt_enable();
+}
+static void delay_cycles(uint32_t cycles)
+{
+    for(uint32_t i =0; i < cycles;i++)
+    {
+        __asm__("nop");
     }
+}
+int main(void){
+    rcc_setup();
+    gpio_setup();
+    systick_setup();
+
+    uint64_t start_time = get_ticks();
+
+    while(1){
+        if (get_ticks()-start_time >= 1000){
+            gpio_toggle(LED_PORT,LED_PIN);
+            start_time = get_ticks();
+        }
+        
+    }
+    // Never return
+    return 0;
 }
