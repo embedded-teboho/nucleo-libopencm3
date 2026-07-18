e3ecdf09f24ae9229983093374e2fb84ac6a4fed
Wana Meshack Teboho <88057845+Teboho-Wana10@users.noreply.github.com>
2026-07-18 10:32:14 +0200

    Add a new python file for sending data

--- projects/03-LCD-UART-I2C/main.c
+++ projects/03-LCD-UART-I2C/main.c
@@ -1,3 +1,17 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/i2c.h>
+
+void rcc_setup(){
+    rcc_periph_clock_enable(RCC_I2C1);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_GPIOB);
+}
+
 int main(void) {
 
     while (1) {
