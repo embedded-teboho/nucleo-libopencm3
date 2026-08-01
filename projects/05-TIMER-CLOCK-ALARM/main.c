e62a37b9278f28d6ccb868520ec4a7ca30cdd2f8
Wana Meshack Teboho <88057845+Teboho-Wana10@users.noreply.github.com>
2026-08-01 11:31:08 +0200

    Add a new folder for the new project

---
+++ projects/05-TIMER-CLOCK-ALARM/main.c
@@ -0,0 +1,13 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+#include <stdbool.h>
+
+int main(void){
+
+    while(1){
+        // Should not be here
+    }
+    
+}
